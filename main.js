e6106242be0cd7d1fc784cf1c9871a8a1e6e27ff
Omar Ashraf <omarashrafothman1234@gmail.com>
2021-07-29 23:52:27 +0200

    init new repo

---
+++ main.js
@@ -0,0 +1,21 @@
+// 1- Comments
+// this is an inline comment
+/*
+	this is multiple line comment
+*/
+
+// 2- variables
+var ticket_price = prompt("what is ticket price?");
+console.log(ticket_price);
+
+// Flow control
+// 1- if conditions
+if (ticket_price < 2000) {
+    console.log("the ticket is excepinsev");
+} else {
+    console.log(" the ticket is sheep");
+}
+
+// 2- switch
+
+// 3- for loop
